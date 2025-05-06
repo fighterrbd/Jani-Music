8ef3b6be95430d09ba845b801bea63d5e1809804
fighterrbd <fighterrbd@gmail.com>
2025-05-06 09:41:47 +0530

    Update play.py

--- EsproMusic/utils/inline/play.py
+++ EsproMusic/utils/inline/play.py
@@ -64,7 +64,7 @@ def stream_markup_timer(_, chat_id, played, dur):
           #  InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
          #   InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
           #  InlineKeyboardButton(text="▣", callback_data=f"ADMIN Stop|{chat_id}"),
-            InlineKeyboardButton(text="➥  ‣‣I 𝗦𝗸𝗶𝗽", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="➥ 𝗡𝗲𝘅𝘁 𝗦𝗼𝗻𝗴 𝗣𝗹𝗮𝘆 ‣‣I 𝗦𝗸𝗶𝗽", callback_data=f"ADMIN Skip|{chat_id}"),
         ],
          [
            #  InlineKeyboardButton(text="σᴡηєʀ", url="https://t.me/Jani_RP_Lover"),
